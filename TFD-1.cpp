11f04381d085aceadc87eaa35b1e4eb4c45a111e
Ange Meli <kindson17@gmail.com>
2025-01-29 14:38:39 +0100

    Update TFD-1.cpp

--- TFD-1.cpp
+++ TFD-1.cpp
@@ -16,6 +16,11 @@ void read_file(string file_name, vector<double> &signaux) {
         signaux.push_back(value); // Pour stocker les données dans un tableau dynamique
     }
     file.close();
+
+    cout << "\nLes valeurs suivantes ont ete recuperees depuis le fichier texte :\n";
+    for (int i = 0; i < signaux.size(); i++) {
+        cout << "\033[32m" << signaux[i] << "\033[37m    ";
+    }
 }
 
 // Fonction de calcul de la DFT Simplifiée
@@ -31,19 +36,57 @@ vector<complex<double>> dft(vector<double> &signaux) {
         }
     }
 
+    cout << "\n\nLe calcul de la DFT est termine. Les resultats seront affiches sous forme de diagramme\n";
     return X;
 }
 
 // Fonction permettant d'afficher les résultats sous forme de diagramme en ASCII
-void display_results() {
+void display_results(vector<double> &signaux, const vector<complex<double>> &results) {
+    int max_amplitude = 0;
+    
+    // Trouver l'amplitude maximale pour la normalisation
+    for (const auto &c : results) {
+        int amplitude = static_cast<int>(abs(c));
+        if (amplitude > max_amplitude) {
+            max_amplitude = amplitude;
+        }
+    }
 
+    // Affichage des amplitudes sous forme de diagramme ASCII
+    cout << "\nDiagramme des amplitudes (les valeurs élevées sont affichées en rouge) :" << "\n\n";
+
+    for (int i = 0; i < results.size(); ++i) {
+        int amplitude = abs(results[i]);
+        int bar_length = amplitude * 50 / max_amplitude; // Normaliser à une longueur de 50 caractères
+        cout << setw(3) << i+1 << " : ";
+        // Détection de pics
+        if (amplitude > 10) {
+            cout << "\033[31m" << string(bar_length, '-') << " (" << amplitude << ")" << "\033[37m\n";
+        }
+        else {
+            cout << string(bar_length, '-') << " (" << amplitude << ")" << "\n";
+        }
+    }
+
+    cout << "\nCorrespondance des signaux : \n";
+    for (int i = 0; i < signaux.size(); i++) {
+        cout << i+1 << " -> " << signaux[i] <<"\n";
+    }
 }
 
 int main(int argc, char const *argv[]) {
+    cout << "\nBienvenue dans ce programme. \nL'objectif est de calculer la DFT pour un ensemble de signaux et d'afficher les resultats sous forme de diagramme en console.\n";
     vector<double> signaux;
     string file_name = "valeurs.txt";
 
     read_file(file_name, signaux);
+
+    vector<complex<double>> results = dft(signaux);
+
+    display_results(signaux, results);
+
+    return 0;
+}
 
     vector<complex<double>> results = dft(signaux);
 
